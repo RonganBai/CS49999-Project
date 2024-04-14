5a5c4a64571b709ed03e4ef4b00c3b2a9c08c4df
RonganBai <140551985+RonganBai@users.noreply.github.com>
2024-04-13 21:50:24 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -3,43 +3,78 @@ import './App.css';
 import Sidebar from './PageFunction/sidebar';
 import RenderCalendar from './PageFunction/HomeCalendar';
 import Register from './PageFunction/register';
+import ChangePIN from './PageFunction/changePIN';
+import Appointment from './PageFunction/Appointment1';
 import SunriseSunsetComponent from './PageFunction/SunriseSunsetComponent';
 import moment from 'moment-timezone';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import AddressInput from './PageFunction/Address'; // 导入 AddressInput 组件
+
+import PageTitle from './PageFunction/pageTitle';
+import Toggle from './PageFunction/Toggle';
+import GoogleCalendarEvents from './PageFunction/GoogleCalendarEvents';
+import Address from './PageFunction/Address';
+import GoogleCalendarEventCreate from './PageFunction/GoogleCalendarEventCreate';
+
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isGoogleLoggedIn, setIsGoogleLoggedIn] = useState(false);
-  const [page, setPage] = useState(localStorage.getItem('currentPage') || 'Home'); // 从本地存储中获取当前页面状态，默认为 'Home'
+  const [page, setPage] = useState(localStorage.getItem('currentPage') || 'Home');
+  const [selectedDate, setSelectedDate] = useState(moment()); // 默认为当天日期
+  const [selectedDate2, setSelectedDate2] = useState(moment());
+  const [selectedLocation, setSelectedLocation] = useState('');
   const [coordinates, setCoordinates] = useState('');
-  const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD')); // 默认为当天日期
-
+  const [isDark, setDark] = useState(false);
   const session = useSession();
   const supabase = useSupabaseClient();
 
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
+  const [parentTime, setParentTime] = useState('AM');
+
+    const handleTimeChange = (newTime) => {
+        setParentTime(newTime);
+    };
+    console.log(parentTime);
+
+    // 定义处理日出日落时间的函数
+    const handleSunriseSunsetTime = (sunriseTime, sunsetTime) => {
+        setSunrise(sunriseTime);
+        setSunset(sunsetTime);
+    };
+
   useEffect(() => {
-  const storedIsLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  setIsLoggedIn(storedIsLoggedIn);
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    setIsLoggedIn(storedIsLoggedIn);
 
-  // 检查谷歌登录状态
-  if (session?.user?.id) {
-    setIsGoogleLoggedIn(true);
-    console.log('Google is logged in');
-  } else {
-    setIsGoogleLoggedIn(false);
-    console.log('Google is not logged in');
-  }
+    const storedGoogleLoggedIn = localStorage.getItem('isGoogleLoggedIn') === 'true';
+    setIsGoogleLoggedIn(storedGoogleLoggedIn);
 
-  // 检查本地存储中的谷歌登录状态
-  const storedGoogleLoggedIn = localStorage.getItem('isGoogleLoggedIn') === 'true';
-  setIsGoogleLoggedIn(storedGoogleLoggedIn);
+    if (session?.provider_token) {
+      setIsGoogleLoggedIn(true);
+      console.log('Google is logged in');
+    } else {
+      if (isGoogleLoggedIn) {
+        console.log("LOGGED IN BUT TOKEN EXPIRED");
+      }
+      console.log('Google is not logged in');
+    }
 
-  // 检查本地存储中的当前页面状态
-  const storedPage = localStorage.getItem('currentPage') || 'Home';
-  setPage(storedPage);
-}, [session, setPage]);
+    const storedIsDark = localStorage.getItem('isDark') === 'true';
+    const storedIsLight = localStorage.getItem('isLight') === 'true';
 
+    if (storedIsDark || storedIsLight) {
+      setDark(storedIsDark);
+    } else {
+      const defaultDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setDark(defaultDarkMode);
+    }
+
+    document.body.classList.add(isDark ? 'dark' : 'light');
+    const storedPage = localStorage.getItem('currentPage') || 'Home';
+    setPage(storedPage);  
+  }, [session, setPage]);
 
   useEffect(() => {
     console.log('Login status changed(App):', isLoggedIn);
@@ -47,28 +82,40 @@ function App() {
 
   async function googleSignIn() {
     try {
-      const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          scopes: 'https://www.googleapis.com/auth/calendar'
+        const { user, session, error: gmailError } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                scopes: [
+                    'https://www.googleapis.com/auth/gmail.send',
+                    'https://www.googleapis.com/auth/calendar'
+                ]
+            }
+        });
+
+        if (gmailError) {
+            alert("Error logging in to Google provider with Supabase for Gmail API");
+            console.error(gmailError);
+            return;
         }
-      });
-      if (error) {
-        throw new Error(error.message); // 抛出错误以处理登录失败
-      } else {
-        setIsGoogleLoggedIn(true);
-        setIsLoggedIn(true); // 在谷歌登录成功后设置为已登录状态
-        localStorage.setItem('isGoogleLoggedIn', 'true');
-      }
+
+        if (user && session) {
+            setIsGoogleLoggedIn(true);
+            localStorage.setItem('isGoogleLoggedIn', 'true');
+           
+        } else {
+            alert("Error logging in to Google provider with Supabase");
+        }
     } catch (error) {
-      alert("Error logging in to Google provider with Supabase: " + error.message); // 提示用户登录失败
-      console.log(error);
+        console.error("Error logging in to Google provider with Supabase:", error.message);
+        alert("Error logging in to Google provider with Supabase");
     }
-  }
+};
+
+
 
   async function googlesignOut() {
     await supabase.auth.signOut();
-    setIsLoggedIn(false); // 添加这行代码
+    setIsLoggedIn(false);
     setIsGoogleLoggedIn(false);
     localStorage.removeItem('isGoogleLoggedIn');
     window.location.reload();
@@ -76,14 +123,34 @@ function App() {
 
   const handleDateSelect = (date) => {
     console.log('Selected date:', date.format('YYYY-MM-DD'));
-    setSelectedDate(date.format('YYYY-MM-DD'));
+    setSelectedDate(date);
   };
+
+  const handleDateSelect2 = (date) => {
+    console.log('Selected date:', date.format('YYYY-MM-DD'));
+    setSelectedDate2(date);
+  };
+
+  const handleDarkToggle = () => {
+    if (isDark) {
+      localStorage.removeItem('isDark');
+      localStorage.setItem('isLight', 'true');
+    } else {
+      localStorage.removeItem('isLight');
+      localStorage.setItem('isDark', 'true');
+    }
+
+    document.body.classList.remove(isDark ? 'dark' : 'light');
+    document.body.classList.add(isDark ? 'light' : 'dark');
+    setDark(!isDark);
+  }
 
   // 处理地址提交
   const handleAddressSubmit = (newCoords) => {
-    console.log('坐标:', newCoords); // 修正此处的变量名为 newCoords
-    setCoordinates(newCoords);
+      console.log('坐标:', newCoords); // 修正此处的变量名为 newCoords
+      setCoordinates(newCoords);
   };
+
 
   return (
     <div className="App">
@@ -94,10 +161,32 @@ function App() {
           <>
             {isGoogleLoggedIn ? (
               <>
+                {page === 'Home' ? (
+                  <div className="home-container">
+                    <PageTitle page={page}/>
+                    {/*<Accordion icon={"qbIcon"} status={"Online"}/>*/}
+                    <div className="calendar-parent-container">
+                        <RenderCalendar selectedDate={selectedDate} onDateSelect={handleDateSelect} />
+                        <div className="side-content-container">
+                          <GoogleCalendarEvents selectedDate={selectedDate}>
+                            <Address onAddressSubmit={handleAddressSubmit} setSelectedLocation={setSelectedLocation}/>
+                            <SunriseSunsetComponent 
+                            coordinates={coordinates} 
+                            selectedDate={selectedDate} 
+                            selectedLocation={selectedLocation}
+                            onSunriseSunsetTime={handleSunriseSunsetTime}/>
+                          </GoogleCalendarEvents>
+                        </div>
+                    </div>
+                  </div>
+                ) : (
+                  <PageTitle page={page}/>
+                )}
                 {page === 'Settings' && (
                   <div className="settings-container">
                     <h2>Settings</h2>
-                    <Register setPage={setPage} />
+                    <Register />
+                    <ChangePIN />
                   </div>
                 )}
                 {page === 'Customer Information Input' && (
@@ -106,22 +195,32 @@ function App() {
                     <p>{`Today is ${moment().format('YYYY-MM-DD')}`}</p>
                   </div>
                 )}
-                {page === 'Home' && (
+                {page === 'Appointment' && (
+                  <div>
                   <div className="calendar-parent-container">
-                    <div className="calendar-container">
-                      <RenderCalendar onDateSelect={handleDateSelect} />
-                    </div>
-                    <div className="address-wooster-container">
-                      <div className="address-container">
-                        <AddressInput onAddressSubmit={handleAddressSubmit} />
-                      </div>
-                      <div className="wooster-time-container pointer-events-none">
-                        <SunriseSunsetComponent
-                          coordinates={coordinates}
-                          selectedDate={selectedDate}
-                        />
-                      </div>
-                    </div>
+                    <RenderCalendar selectedDate={selectedDate} onDateSelect={handleDateSelect2} />
+                    <Appointment selectedDate2={selectedDate2} 
+                    sunrise={sunrise} 
+                    sunset={sunset}
+                    selectedLocation={selectedLocation}
+                    parentTime={parentTime}
+                    onTimeChange={handleTimeChange}/>
+                  </div>
+                  <div className="side_appointment_page">
+                  <Address onAddressSubmit={handleAddressSubmit} setSelectedLocation={setSelectedLocation}/>
+                  <SunriseSunsetComponent 
+  coordinates={coordinates} 
+  selectedDate={selectedDate2} 
+  selectedLocation={selectedLocation} 
+  onSunriseSunsetTime={handleSunriseSunsetTime}
+/>
+<GoogleCalendarEventCreate 
+                    parentTime={parentTime}
+                    selectedDate2={selectedDate2}
+                    selectedLocation={selectedLocation}
+                    sunrise={sunrise} 
+                    sunset={sunset}/>
+                  </div>
                   </div>
                 )}
               </>
@@ -131,17 +230,23 @@ function App() {
                 <button onClick={googleSignIn} className="logon-button">Google Sign In</button>
               </div>
             )}
-            <button 
-              onClick={isGoogleLoggedIn ? googlesignOut : googleSignIn} 
-              className="google-signin-button"
-              style={{ backgroundColor: isGoogleLoggedIn ? 'green' : 'red' }}
-            >
-              {isGoogleLoggedIn ? 'Google Sign Out' : 'Google Sign In'}
-            </button>
+            <div className="account-container">
+              <button 
+                onClick={isGoogleLoggedIn ? googlesignOut : googleSignIn} 
+                className="google-signin-button"
+                style={{ backgroundColor: isGoogleLoggedIn ? 'green' : 'red' }}
+              >
+                {isGoogleLoggedIn ? 'Google Sign Out' : 'Google Sign In'}
+              </button>
+              <button
+                className="qb-signin-button">
+                  QuickBooks Sign In
+              </button>
+              <Toggle status={ isDark } method={ handleDarkToggle }/>
+            </div>
           </>
         ) : (
-          <div>
-          </div>
+          <div></div>
         )}
       </div>
     </div>
